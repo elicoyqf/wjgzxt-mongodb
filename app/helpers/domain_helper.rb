6673f78d762d18cc637f60acc600ac5d8909bfd9
elico <elico_yqf@sina.com>
2013-10-24 23:06:16 +0800

    修正域名查重的问题。

--- app/helpers/domain_helper.rb
+++ app/helpers/domain_helper.rb
@@ -1,14 +1,32 @@
 #encoding : utf-8
 module DomainHelper
   def rand_domain(locale)
-    tmp_data = nil
+    tmp_data = []
+    TestDestNode.all.each do |tdn|
+      if contrast_locale tdn.locale.to_s, locale
+        tmp_data << tdn
+      end
+    end
+
+=begin
     if locale != '其它'
       #tmp_data = TestDestNode.where('locale = ?', locale)
-      tmp_data = TestDestNode.where(:locale => locale)
+      #tmp_data = TestDestNode.where(:locale => locale)
+      TestDestNode.all.each do |tdn|
+        if contrast_locale tdn.locale.to_s, locale
+          tmp_data << tdn
+        end
+      end
     else
       #tmp_data = TestDestNode.where('locale not in (?)', ['电信', '联通', '移动', '铁通'])
-      tmp_data = TestDestNode.where(:locale.nin => ['电信', '联通', '移动', '铁通'])
+      #tmp_data = TestDestNode.where(:locale.nin => ['电信', '联通', '移动', '铁通'])
+      TestDestNode.all.each do |tdn|
+        if contrast_locale tdn.locale.to_s, locale
+          tmp_data << tdn
+        end
+      end
     end
+=end
 
     out_arr = Set.new
     out_set = Set.new
