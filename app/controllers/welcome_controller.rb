55d743457475bf1bd887051edd10fe6bd4ad0a5a
elico <elico_yqf@sina.com>
2013-10-26 15:37:17 +0800

    修正时区的问题。

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -13,7 +13,7 @@ class WelcomeController < ApplicationController
     puts '-'*50
 
     @time_begin = Time.now.at_beginning_of_day
-    @time_end   = Time.now 
+    @time_end   = Time.now
 
     @dx, @lt, @oe, @total_pos, @total_neg, @total_eql, @dx_array, @lt_array = cal_export_ranking time_begin, time_end, ef
     render layout: 'application'
