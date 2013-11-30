75ff17acefb50ea5d5b3d65ffa9902a6b2bd0aac
elico <elico_yqf@sina.com>
2013-11-30 14:25:56 +0800

    修正了时区的问题。

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,18 +4,11 @@ class WelcomeController < ApplicationController
     ef          = ExportName.where(user_id: current_user.id, status: 0)
 
     #[dx,lt,oe,total_pos,total_neg,total_eql,dx_array,lt_array]
-    time_begin = Time.now.at_beginning_of_day + 8.hour
-    time_end   = Time.now + 8.hour
-
-    puts '-'*50
-    puts time_begin
-    puts time_end
-    puts '-'*50
 
     @time_begin = Time.now.at_beginning_of_day
     @time_end   = Time.now
 
-    @dx, @lt, @oe, @total_pos, @total_neg, @total_eql, @dx_array, @lt_array = cal_export_ranking time_begin, time_end, ef
+    @dx, @lt, @oe, @total_pos, @total_neg, @total_eql, @dx_array, @lt_array = cal_export_ranking @time_begin, @time_end, ef
     render layout: 'application'
   end
 end
